8ddf91e8f3ce950bc87c440b0b06502c80bdc7a0
handsofblue67 <michael.robison168@gmail.com>
2016-04-19 15:44:46 -0600

    clears screen, more human readable

--- index.js
+++ index.js
@@ -4,24 +4,31 @@ var _ = require('lodash');
 var savedQuestions = [];
 
 setInterval(function() {
-  // set id equal to your student id
-  http.get('http://tutorhelp.uvu.edu/api_data.php?action=getquestions&tutorid=', (res) => {
-    res.setEncoding('utf8');
-    res.on('data', (chunk) => {
-      var data = JSON.parse(chunk);
-      console.log(data.questions);
-      if (_.differenceWith(data.questions, savedQuestions, _.isEqual).length > 0) {
-        console.log(_.differenceWith(data.questions, savedQuestions, _.isEqual));
-        process.stdout.write('\x07');
-        savedQuestions = _.cloneDeep(data.questions);
-        console.log('saved');
-        console.log(savedQuestions);
-      }
+    // set id equal to your student id
+    http.get('http://tutorhelp.uvu.edu/api_data.php?action=getquestions&tutorid=', (res) => {
+        res.setEncoding('utf8');
+        res.on('data', (chunk) => {
+            var data = JSON.parse(chunk);
+            if (_.differenceWith(data.questions, savedQuestions, _.isEqual).length > 0) {
+                console.log('\033c');
+                process.stdout.write('\x07');
+                savedQuestions = _.cloneDeep(data.questions);
+                console.log("Name\t\t\tClass\t\tTable\t\tQuestion");
+                console.log("-----------------------------------------------------------------");
+                savedQuestions.forEach(o => {
+                    console.log(`${o.studentName}\t\t${o.class}\t${o.table}\t\t${o.question}`);
+                });
+            } else if (_.differenceWith(savedQuestions, data.questions, _.isEqual).length > 0) {
+                console.log('\033c');
+                savedQuestions = _.cloneDeep(data.questions);
+                console.log("Name\t\t\tClass\t\tTable\t\tQuestion");
+                console.log("-----------------------------------------------------------------");
+                savedQuestions.forEach(o => {
+                    console.log(`${o.studentName}\t\t${o.class}\t${o.table}\t\t${o.question}`);
+                });
+            }
+        });
+    }).on('error', (e) => {
+        console.log(`Got error: ${e.message}`);
     });
-    res.on('end', () => {
-      console.log('No more data in response.')
-    })
-  }).on('error', (e) => {
-    console.log(`Got error: ${e.message}`);
-  });
 }, 5000);
