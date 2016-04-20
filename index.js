23035b51f85f0ce46d00adfdd7cb7d31683a5922
handsofblue67 <michael.robison168@gmail.com>
2016-04-19 18:15:40 -0600

    factored some stuff

--- index.js
+++ index.js
@@ -3,32 +3,29 @@ var _ = require('lodash');
 
 var savedQuestions = [];
 
+function printQuestions(questions) {
+    console.log('\033c');
+    savedQuestions = _.cloneDeep(questions);
+    console.log("Name\t\t\t\tClass\t\t\tTable\t\t\tQuestion");
+    console.log("-----------------------------------------------------------------");
+    savedQuestions.forEach(o => {
+        console.log(`${o.studentName}\t\t\t${o.class}\t\t${o.table}\t\t\t${o.question}`);
+    });
+}
+
 setInterval(function() {
     // set id equal to your student id
-    http.get('http://tutorhelp.uvu.edu/api_data.php?action=getquestions&tutorid=', (res) => {
+    http.get('http://tutorhelp.uvu.edu/api_data.php?action=getquestions&tutorid=10704736', (res) => {
         res.setEncoding('utf8');
         res.on('data', (chunk) => {
             var data = JSON.parse(chunk);
+            // TODO: change these condidtions to a more simple and lightweight one
             if (_.differenceWith(data.questions, savedQuestions, _.isEqual).length > 0) {
-                console.log('\033c');
                 process.stdout.write('\x07');
-                savedQuestions = _.cloneDeep(data.questions);
-                console.log("Name\t\t\tClass\t\tTable\t\tQuestion");
-                console.log("-----------------------------------------------------------------");
-                savedQuestions.forEach(o => {
-                    console.log(`${o.studentName}\t\t${o.class}\t${o.table}\t\t${o.question}`);
-                });
+                printQuestions(data.questions);
             } else if (_.differenceWith(savedQuestions, data.questions, _.isEqual).length > 0) {
-                console.log('\033c');
-                savedQuestions = _.cloneDeep(data.questions);
-                console.log("Name\t\t\tClass\t\tTable\t\tQuestion");
-                console.log("-----------------------------------------------------------------");
-                savedQuestions.forEach(o => {
-                    console.log(`${o.studentName}\t\t${o.class}\t${o.table}\t\t${o.question}`);
-                });
+                printQuestions(data.Questions);
             }
         });
-    }).on('error', (e) => {
-        console.log(`Got error: ${e.message}`);
-    });
+    }).on('error', (e) => { console.log(`Got error: ${e.message}`); });
 }, 5000);
