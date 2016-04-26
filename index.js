70e0804a9ad8a3d91226217158ec65979ae00065
Michael <michael.robison168@gmail.com>
2016-04-26 15:53:16 -0600

    can now help and finish helping

--- index.js
+++ index.js
@@ -7,31 +7,66 @@ var _ = require('lodash');
 var fs = require('fs');
 var http = require('http');
 const readline = require('readline');
+
+
 // TODO: use enpoints and this readline interface to respond and finish answering questions.
 const rl = readline.createInterface({
     input: process.stdin,
-     output: process.stout
+      output: process.stout
 });
-
-rl.on('line', cmd => { console.log(`You just typed ${cmd}`); });
 
 var savedQuestions = [];
 var uvid = '';
+var tutorHelping = 0;
 
-function helpStudent() {
+getQueue();
 
+rl.on('line', cmd => { 
+    var index = parseInt(cmd) + 1;
+    if ( tutorHelping ) {
+        http.get(`http://tutorhelp.uvu.edu/api_data.php?action=finishquestion&questionId=${tutorHelping}`, (res) => {
+            res.setEncoding('utf8');
+            tutorHelping = 0;
+            getQueue();
+            });
+    } else {
+        if ( index < savedQuestions.length ) {
+            console.log('\033c');
+            http.get(`http://tutorhelp.uvu.edu/api_data.php?action=respondquestion&questionId=${savedQuestions[index].id}&tutorUvid=${uvid}`, (res) => {
+                res.setEncoding('utf8');
+                res.on('data', (chunk) => { console.log(`Helping ${savedQuestions[index].studentName}`) }); 
+                tutorHelping = savedQuestions[index].id;
+                });
+        }
+    }
+});
+
+function getQueue() {
+    if ( tutorHelping ) { return; }
+    // set id equal to your student id
+    http.get('http://tutorhelp.uvu.edu/api_data.php?action=getquestions&tutorid=' + uvid, (res) => {
+            res.setEncoding('utf8');
+            res.on('data', (chunk) => {
+                var data = JSON.parse(chunk);
+                // TODO: change these condidtions to a more simple and lightweight one
+                if ( !data.questions.length ) { printQuestions(data.questions); }
+                if ( _.differenceWith(data.questions, savedQuestions, _.isEqual ).length > 0) {
+                    process.stdout.write('\x07');
+                    printQuestions(data.questions);
+                } else if ( _.differenceWith(savedQuestions, data.questions, _.isEqual ).length > 0) {
+                    printQuestions(data.questions);
+                }
+            });
+            }).on('error', (e) => { console.log(`Got error: ${e.message}`); });
 }
 
-function finishHelpingStudent() {
-
-}
 
 function printQuestions(questions) {
     console.log('\033c');
     savedQuestions = _.cloneDeep(questions);
     console.log("Name\t\t\t\tClass\t\t\tTable\t\t\tQuestion");
     console.log("-----------------------------------------------------------------");
-    _.forEach(savedQuestions, o => {
+    _.forEach(questions, o => {
         console.log(`${o.studentName}\t\t\t${o.class}\t\t${o.table}\t\t\t${o.question}`);
     });
 }
@@ -41,23 +76,4 @@ fs.readFile('uvid.txt', (err, data) => {
     uvid = data.toString();
 });
 
-setInterval(function() {
-    // set id equal to your student id
-    http.get('http://tutorhelp.uvu.edu/api_data.php?action=getquestions&tutorid=' + uvid, (res) => {
-        res.setEncoding('utf8');
-        res.on('data', (chunk) => {
-            var data = JSON.parse(chunk);
-            // TODO: change these condidtions to a more simple and lightweight one
-            if (_.differenceWith(data.questions, savedQuestions, _.isEqual).length > 0) {
-                process.stdout.write('\x07');
-                printQuestions(data.questions);
-            } else if (_.differenceWith(savedQuestions, data.questions, _.isEqual).length > 0) {
-                printQuestions(data.Questions);
-            }
-        });
-        }).on('error', (e) => { console.log(`Got error: ${e.message}`); });
-}, 5000);
-
-
-
-
+setInterval(getQueue, 5000);
